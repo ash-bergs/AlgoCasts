2e5d62957f59e33f7ecd0916c17c52356ab5d811
Ash <ashley.bergsma42@gmail.com>
2021-05-24 00:03:06 -0700

    Adds first solution

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -12,6 +12,20 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+function fizzBuzz(n) {
+    // 1. Start small: Let's get the numbers 1 - n to print! 
+    for (let i = 1; i <= n; i++){
+        // 2. Now we need to introduce the special cases... the question here is what values to check for first 
+        // 15 will be both a multiple of 3 and 5, for example - so i think we check for multiples of both, then check for multiples of 5, then 3
+        //* in decreasing value 👆 
+        if (i % 15 === 0){
+            console.log("fizzbuzz");
+        } else if (i % 3 ===0){
+            console.log("fizz"); 
+        } else if(i % 5 === 0){
+            console.log("buzz")
+        } else console.log(i); 
+    }
+}
 
 module.exports = fizzBuzz;
